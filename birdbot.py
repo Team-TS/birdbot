9fcb45e97c6b83ddf8f652ab0ac595ec570e539d
Birdtalon <birdtalon@gmail.com>
2018-09-26 19:01:30 +0100

    voice attempt

--- birdbot.py
+++ birdbot.py
@@ -2,6 +2,7 @@ import logging
 from discord import *
 import yaml
 import gvars
+import asyncio
 
 logging.basicConfig(level=logging.INFO)
 
@@ -16,10 +17,28 @@ client = Client()
 voiceclient = None
 vplayer = None
 
+playqueue = []
+
 # Discord token
 token = config["discord"]["token"]
 
 mprefix = "£"
+
+async def checkplayback():
+    global vplayer
+    while vplayer and voiceclient:
+        if not vplayer:
+            return
+        if voiceclient and vplayer.isdone() and len(playqueue):
+            vplayer = await voiceclient.create_ytdl_player(next(iter(playqueue)))
+        else:
+            vplayer = None
+        asyncio.sleep(5)
+
+async def enqueue(song):
+    global playqueue
+    playqueue.append(song)
+    return
 
 @client.event
 async def on_ready():
@@ -41,6 +60,7 @@ async def on_message(message : Message):
     # VOICE STUFF
 
     global voiceclient
+    global vplayer
 
     if command == "join":
         if message.author.voice.voice_channel:
@@ -61,10 +81,29 @@ async def on_message(message : Message):
         
         music = cargs[0]
 
+        if vplayer and vplayer.is_playing():
+            await enqueue(music)
+            return await client.send_message(message.channel, "Enqueued.")
+
         vplayer = await voiceclient.create_ytdl_player(music)
         vplayer.start()
 
         return await client.send_message(message.channel, "Now playing: {0}".format(vplayer.title))
+
+    if command == "stop":
+        if not vplayer or not vplayer.is_playing():
+            return await client.send_message(message.channel, "I am not playing anything!")
+
+        voiceclient.stop()
+    
+    if command == "volume":
+        if not vplayer:
+            return
+
+        try:
+            vplayer.volume = int(cargs[0])
+        except Exception as e:
+            return await client.send_message("ERROR: SKREK! : {0}".format(e))
 
     # Utilities
 
@@ -82,12 +121,10 @@ async def on_member_join(member : Member):
 
 @client.event
 async def on_voice_state_update(before, after):
-    print("on_voice_state_update called")
     bchan = before.voice.voice_channel
     achan = after.voice.voice_channel
 
     if achan == bchan:
-        print("Same, returning")
         return
 
     return await client.send_message(before.server.get_channel(gvars.voicelog), "{0} has switched from {1} to {2}".format(before.name, bchan, achan))
